927ecd04a5026353ee771896767f5437354984ea
Mercy <mercycheruiyot2013@gmail.com>
2022-12-09 17:26:42 +0300

    Added build

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,14 +1,20 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+
+  config.hosts <<
+  IPAddr.new("0.0.0.0/0"),
+  IPAddr.new("::/0")
+  "localhost",
+  ENV["RAILS_DEVELOPMENT_HOSTS"] 
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
-  config.hosts <<
-  "bookstore_backend.production.up.railway.up"
+  # config.hosts <<
+  # "bookstore-backend.production.up.railway.up"
 
   # Do not eager load code on boot.
   config.eager_load = false
