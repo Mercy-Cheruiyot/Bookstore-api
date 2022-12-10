118f9ae5a6e1d6c78d03bb0e9f80137308fe2767
Mercy <mercycheruiyot2013@gmail.com>
2022-12-10 16:41:38 +0300

    Added Cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins 'https://remarkable-cendol-e74920.netlify.app'
-      resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
-    end
+  allow do
+    origins '*'
+    resource '*', 
+    headers: :any,
+    methods: [:create, :get, :post, :put, :patch, :delete, :options, :head]
   end
+end
